79cf955018813af6540816cd3376222dd253f162
Mattichs <50959786+Mattichs@users.noreply.github.com>
2025-04-27 09:52:59 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -48,7 +48,7 @@ int main() {
         cerr << "Errore: Impossibile aprire il file per salvare il vocabolario: " << vocabPath << endl;
     }
 
-    // Istogrammi
+    // Histograms
     Mat trainData;
     for (const auto& desc : allDescriptors) {
         Mat hist = computeHistogram(desc, vocabulary);
@@ -62,36 +62,32 @@ int main() {
     //cout << "Dimensioni TrainData: " << trainData.size() << " Tipo: " << trainData.type() << endl;
     //cout << "Dimensioni LabelsMat: " << labelsMat.size() << " Tipo: " << labelsMat.type() << endl;
 
-    // 2. Creazione e configurazione del classificatore Random Forest
+    // Random Forest setup
     Ptr<ml::RTrees> rf = ml::RTrees::create();
 
-    // Imposta i parametri (questi sono valori di esempio, potresti doverli aggiustare)
-    rf->setMaxDepth(1000);          // Profondità massima degli alberi
-    rf->setMinSampleCount(5);     // Numero minimo di campioni per splittare un nodo
-    rf->setRegressionAccuracy(0); // Impostare a 0 per la classificazione
-    rf->setUseSurrogates(false);  // Di solito non necessario
-    rf->setCVFolds(0);            // Numero di fold per cross-validation (0 per non usarla durante il training)
+    rf->setMaxDepth(1000);          
+    rf->setMinSampleCount(5);    
+    rf->setRegressionAccuracy(0); 
+    rf->setUseSurrogates(false);  
+    rf->setCVFolds(0);            
     rf->setUse1SERule(false);
     rf->setTruncatePrunedTree(false);
-    // rf->setPriors(Mat());      // Puoi impostare priori se le classi sono sbilanciate
-    rf->setCalculateVarImportance(true); // Calcola l'importanza delle feature (visual words)
-    rf->setActiveVarCount(0);     // Numero di feature da considerare ad ogni split (0 = usa sqrt(numero totale feature))
-
-    // Criteri di terminazione per l'addestramento degli alberi
+    rf->setCalculateVarImportance(true); 
+    rf->setActiveVarCount(0);    
     rf->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER + TermCriteria::EPS, 1000, 0.01));    
 
-    // 3. Addestramento del modello
+    // Train the model
     cout << "Addestramento Random Forest..." << endl;
     try {
         rf->train(trainData, ml::ROW_SAMPLE, labelsMat);
          cout << "Addestramento completato." << endl;
     } catch (const cv::Exception& e) {
         cerr << "Errore durante l'addestramento della Random Forest: " << e.what() << endl;
-        return -1; // O gestisci l'errore
+        return -1; 
     }
 
 
-    // 4. Salvare il modello addestrato
+    // Save model
     string modelPath = "../temp/random_forest_bow_model.yml";
     cout << "Salvataggio del modello in: " << modelPath << endl;
     rf->save(modelPath);
